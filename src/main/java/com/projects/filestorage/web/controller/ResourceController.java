087950f06137c102e2cd72a7cc7e34100f5cfbfd
Max Vdovin <maxpostupaet@gmail.com>
2025-07-27 14:04:19 +0300

    feat: add response status for deleteResource method

--- src/main/java/com/projects/filestorage/web/controller/ResourceController.java
+++ src/main/java/com/projects/filestorage/web/controller/ResourceController.java
@@ -4,11 +4,13 @@ import com.projects.filestorage.service.MinioClientService;
 import com.projects.filestorage.web.dto.response.ResourceInfoDto;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,11 +24,13 @@ public class ResourceController {
     private final MinioClientService minioClientService;
 
     @GetMapping("/resource")
+    @ResponseStatus(HttpStatus.OK)
     public ResourceInfoDto getResourceInfo(@RequestParam("path") String path) {
         return minioClientService.getResourceInfo(path);
     }
 
     @DeleteMapping("/resource")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteResource(@RequestParam("path") String path) {
         minioClientService.deleteResource(path);
     }
