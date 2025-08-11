7bc4112e454340bcacfed097648bace995f0d841
Max Vdovin <maxpostupaet@gmail.com>
2025-08-11 12:25:29 +0300

    feat: add error response dto and integrate it in global exception handler

--- src/main/java/com/projects/filestorage/web/controller/GlobalExceptionHandler.java
+++ src/main/java/com/projects/filestorage/web/controller/GlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import com.projects.filestorage.exception.UnauthenticatedAccessException;
 import com.projects.filestorage.exception.UserAlreadyExistsException;
 import com.projects.filestorage.exception.UserNotFoundException;
 import com.projects.filestorage.exception.UserRoleNotFoundException;
+import com.projects.filestorage.web.dto.response.ErrorResponseDto;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
@@ -26,7 +27,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -34,7 +34,7 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         log.warn("[Handle] Validation error (MethodArgumentNotValidException): {}", ex.getMessage());
         var message = ex.getBindingResult().getFieldErrors().stream()
                 .map(FieldError::getDefaultMessage)
@@ -44,7 +44,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException ex) {
+    public ResponseEntity<ErrorResponseDto> handleConstraintViolationException(ConstraintViolationException ex) {
         log.warn("[Handle] Validation error (ConstraintViolationException): {}", ex.getMessage());
         var message = ex.getConstraintViolations().stream()
                 .map(ConstraintViolation::getMessage)
@@ -54,144 +54,144 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<Map<String, String>> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+    public ResponseEntity<ErrorResponseDto> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
         log.warn("[Handle] Registration conflict (UserAlreadyExistsException): {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<Map<String, String>> handleBadCredentialsException(BadCredentialsException ex) {
+    public ResponseEntity<ErrorResponseDto> handleBadCredentialsException(BadCredentialsException ex) {
         log.warn("[Handle] Authorization error (BadCredentialsException): {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(UnauthenticatedAccessException.class)
-    public ResponseEntity<Map<String, String>> handleUnauthenticatedAccessException(UnauthenticatedAccessException ex) {
+    public ResponseEntity<ErrorResponseDto> handleUnauthenticatedAccessException(UnauthenticatedAccessException ex) {
         log.warn("[Handle] Unauthenticated access (UnauthenticatedAccessException): {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(UserRoleNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleUserRoleNotFoundException(UserRoleNotFoundException ex) {
+    public ResponseEntity<ErrorResponseDto> handleUserRoleNotFoundException(UserRoleNotFoundException ex) {
         log.warn("[Handle] User role not found (UserRoleNotFoundException): {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleUserNotFoundException(UserNotFoundException ex) {
+    public ResponseEntity<ErrorResponseDto> handleUserNotFoundException(UserNotFoundException ex) {
         log.warn("[Handle] User not found (UserNotFoundException): {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<Map<String, String>> handleNoResourceFoundException(NoResourceFoundException ex) {
+    public ResponseEntity<ErrorResponseDto> handleNoResourceFoundException(NoResourceFoundException ex) {
         log.warn("[Handle] Resource not found (NoResourceFoundException): {}", ex.getMessage());
         return handleNotFoundException(ex.getMessage());
     }
 
     @ExceptionHandler(InvalidResourcePathFormatException.class)
-    public ResponseEntity<Map<String, String>> handleInvalidResourcePathFormatException(
+    public ResponseEntity<ErrorResponseDto> handleInvalidResourcePathFormatException(
             InvalidResourcePathFormatException ex) {
         log.warn("[Handle] Validation error (InvalidResourcePathFormatException): {}", ex.getMessage());
         return buildValidationErrorResponse(ex.getMessage());
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleResourceNotFoundException(ResourceNotFoundException ex) {
+    public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException ex) {
         log.warn("[Handle] Resource not found (ResourceNotFoundException): {}", ex.getMessage());
         return handleNotFoundException(ex.getMessage());
     }
 
     @ExceptionHandler(ResourceAlreadyExistsException.class)
-    public ResponseEntity<Map<String, String>> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
+    public ResponseEntity<ErrorResponseDto> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
         log.warn("[Handle] Resource conflict (ResourceAlreadyExistsException): {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(InvalidSearchQueryFormatException.class)
-    public ResponseEntity<Map<String, String>> handleInvalidSearchQueryFormatException(
+    public ResponseEntity<ErrorResponseDto> handleInvalidSearchQueryFormatException(
             InvalidSearchQueryFormatException ex) {
         log.warn("[Handle] Invalid search query format (InvalidSearchQueryFormatException): {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(InvalidMultipartFileException.class)
-    public ResponseEntity<Map<String, String>> handleInvalidMultipartFileException(InvalidMultipartFileException ex) {
+    public ResponseEntity<ErrorResponseDto> handleInvalidMultipartFileException(InvalidMultipartFileException ex) {
         log.warn("[Handle] Invalid multipart file (InvalidMultipartFileException): {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<Map<String, String>> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
+    public ResponseEntity<ErrorResponseDto> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
         log.warn("[Handle] The file size is too large when uploading (MaxUploadSizeExceededException): {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(DirectoryNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleDirectoryNotFoundException(DirectoryNotFoundException ex) {
+    public ResponseEntity<ErrorResponseDto> handleDirectoryNotFoundException(DirectoryNotFoundException ex) {
         log.warn("[Handle] Directory not found (DirectoryNotFoundException): {}", ex.getMessage());
         return handleNotFoundException(ex.getMessage());
     }
 
     @ExceptionHandler(DirectoryDeletionException.class)
-    public ResponseEntity<Map<String, String>> handleDirectoryDeletionException(DirectoryDeletionException ex) {
+    public ResponseEntity<ErrorResponseDto> handleDirectoryDeletionException(DirectoryDeletionException ex) {
         log.warn("[Handle] Error when deleting a directory (DirectoryDeletionException): {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(MinioAccessException.class)
-    public ResponseEntity<Map<String, String>> handleMinioAccessException(MinioAccessException ex) {
+    public ResponseEntity<ErrorResponseDto> handleMinioAccessException(MinioAccessException ex) {
         log.warn("[Handle] Error when working with MinIO: {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(MinioResourceHandlerNotFound.class)
-    public ResponseEntity<Map<String, String>> handleMinioResourceHandlerNotFound(MinioResourceHandlerNotFound ex) {
+    public ResponseEntity<ErrorResponseDto> handleMinioResourceHandlerNotFound(MinioResourceHandlerNotFound ex) {
         log.warn("[Handle] Error when searching for a handler for request to a minio: {}", ex.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Map.of("message", ex.getMessage()));
+                .body(new ErrorResponseDto(ex.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleException(Exception ex) {
+    public ResponseEntity<ErrorResponseDto> handleException(Exception ex) {
         log.error("[Handle] Unexpected error (Exception): {}", ex.getMessage(), ex);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Map.of("message", "Internal error"));
+                .body(new ErrorResponseDto("Internal error"));
     }
 
-    private ResponseEntity<Map<String, String>> handleNotFoundException(String message) {
+    private ResponseEntity<ErrorResponseDto> handleNotFoundException(String message) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(Map.of("message", message));
+                .body(new ErrorResponseDto(message));
     }
 
-    private ResponseEntity<Map<String, String>> buildValidationErrorResponse(String message) {
+    private ResponseEntity<ErrorResponseDto> buildValidationErrorResponse(String message) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of("message", message));
+                .body(new ErrorResponseDto(message));
     }
 }
