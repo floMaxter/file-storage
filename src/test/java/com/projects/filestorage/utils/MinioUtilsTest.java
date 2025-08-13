6396e65b9fb2c66eda9f019c4d1c96ade3a9c73d
Max Vdovin <maxpostupaet@gmail.com>
2025-08-13 15:47:55 +0300

    fix: change test for MinioUtils according to the new app architecture

--- src/test/java/com/projects/filestorage/utils/MinioUtilsTest.java
+++ src/test/java/com/projects/filestorage/utils/MinioUtilsTest.java
@@ -8,59 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MinioUtilsTest {
 
-//    @ParameterizedTest(name = "extractResourceName(\"{0}\" => \"{1}\"")
-//    @MethodSource("com.projects.filestorage.testdata.data.MinioUtilsData#resourceNameExtractionData")
-//    public void extractResourceName_ValidPath_ShouldReturnEmptyName(String path, String expectedName) {
-//        var extractResourceName = MinioUtils.extractResourceName(path);
-//        assertThat(extractResourceName).isEqualTo(expectedName);
-//    }
-//
-//    @ParameterizedTest(name = "extractParentPath(\"{0}\" => \"{1}\"")
-//    @MethodSource("com.projects.filestorage.testdata.data.MinioUtilsData#parentPathExtractionData")
-//    public void extractParentPath_ValidPath_ShouldReturnExpectedParent(String path, String expectedParent) {
-//        var extractParentPath = MinioUtils.extractParentPath(path);
-//        assertThat(extractParentPath).isEqualTo(expectedParent);
-//    }
-//
-//    @ParameterizedTest(name = "Validate the correct path: (\"{0}\")")
-//    @MethodSource("com.projects.filestorage.testdata.data.MinioUtilsData#getValidPath")
-//    public void isValidPathFormat_ValidPath_ShouldReturnTrue(String path) {
-//        var result = MinioUtils.isValidPathFormat(path);
-//        assertThat(result).isTrue();
-//    }
-//
-//    @ParameterizedTest(name = "Validate the incorrect path: (\"{0}\")")
-//    @MethodSource("com.projects.filestorage.testdata.data.MinioUtilsData#getInvalidPath")
-//    public void isValidPathFormat_InvalidPath_ShouldReturnFalse(String path) {
-//        var result = MinioUtils.isValidPathFormat(path);
-//        assertThat(result).isFalse();
-//    }
-//
-//    @ParameterizedTest(name = "Validate the correct directory path: (\"{0}\")")
-//    @MethodSource("com.projects.filestorage.testdata.data.MinioUtilsData#getValidDirectoryPath")
-//    public void isValidDirectoryPathFormat_ValidPath_ShouldReturnTrue(String path) {
-//        var result = MinioUtils.isValidDirectoryPathFormat(path);
-//        assertThat(result).isTrue();
-//    }
-//
-//    @ParameterizedTest(name = "Validate the incorrect directory path: (\"{0}\")")
-//    @MethodSource("com.projects.filestorage.testdata.data.MinioUtilsData#getInvalidDirectoryPath")
-//    public void isValidDirectoryPathFormat_InvalidPath_ShouldReturnFalse(String path) {
-//        var result = MinioUtils.isValidDirectoryPathFormat(path);
-//        assertThat(result).isFalse();
-//    }
-//
-//    @ParameterizedTest(name = "Validate the correct search query: (\"{0}\")")
-//    @MethodSource("com.projects.filestorage.testdata.data.MinioUtilsData#getValidSearchQuery")
-//    public void isValidSearchQueryFormat_ValidQuery_ShouldReturnTrue(String query) {
-//        var result = MinioUtils.isValidSearchQueryFormat(query);
-//        assertThat(result).isTrue();
-//    }
-//
-//    @ParameterizedTest(name = "Validate the incorrect search query: (\"{0}\")")
-//    @MethodSource("com.projects.filestorage.testdata.data.MinioUtilsData#getInvalidSearchQuery")
-//    public void isValidSearchQueryFormat_InvalidQuery_ShouldReturnFalse(String query) {
-//        var result = MinioUtils.isValidSearchQueryFormat(query);
-//        assertThat(result).isFalse();
-//    }
+    @ParameterizedTest(name = "extractResourceName(\"{0}\" => \"{1}\"")
+    @MethodSource("com.projects.filestorage.testdata.data.MinioUtilsData#resourceNameExtractionData")
+    public void extractResourceName_ValidPath_ShouldReturnEmptyName(String path, String expectedName) {
+        var extractResourceName = MinioUtils.extractResourceName(path);
+        assertThat(extractResourceName).isEqualTo(expectedName);
+    }
+
+    @ParameterizedTest(name = "extractParentPath(\"{0}\" => \"{1}\"")
+    @MethodSource("com.projects.filestorage.testdata.data.MinioUtilsData#parentPathExtractionData")
+    public void extractParentPath_ValidPath_ShouldReturnExpectedParent(String path, String expectedParent) {
+        var extractParentPath = MinioUtils.extractParentPath(path);
+        assertThat(extractParentPath).isEqualTo(expectedParent);
+    }
 }
